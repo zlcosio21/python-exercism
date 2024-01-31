edac53e430aba51ca736a49089368e42d676eff2
zlcosio21 <sebas60621@gmail.com>
2024-01-30 21:43:12 -0500

    Collatz-conjecture

---
+++ collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,12 @@
+def steps(number):
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    num_steps = 0
+
+    while number != 1:
+        number = number / 2 if number % 2 == 0 else (number * 3) + 1
+
+        num_steps += 1
+
+    return num_steps
