83f673a7370714bc64931637c50057b33208f77a
zlcosio21 <sebas60621@gmail.com>
2025-03-05 19:42:55 -0500

    Resister Color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+NAME_COLORS = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def color_code(color):
+    return NAME_COLORS.index(color)
+
+
+def colors():
+    return NAME_COLORS
